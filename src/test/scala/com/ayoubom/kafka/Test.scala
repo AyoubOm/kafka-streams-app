da8cf28f070c2f03748c124611138e9de9e9da22
Ayoub Omari <ayoubomari1@outlook.fr>
2024-03-01 11:09:27 +0100

    window topology

---
+++ src/test/scala/com/ayoubom/kafka/Test.scala
@@ -0,0 +1,111 @@
+package com.ayoubom.kafka
+
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.kstream._
+import org.apache.kafka.streams._
+import org.apache.kafka.streams.state.internals.RocksDbWindowBytesStoreSupplier
+import org.scalatest.funsuite.AnyFunSuite
+
+import java.time.{Duration, Instant}
+
+class Test extends AnyFunSuite {
+
+  test("test driver") {
+    val testDriver: TopologyTestDriver = new TopologyTestDriver(topology)
+    val inputTopic = testDriver.createInputTopic("input-topic", new StringSerializer, new IntegerSerializer)
+    val outputTopic = testDriver.createOutputTopic("output-topic", new StringDeserializer, new IntegerDeserializer)
+
+    inputTopic.pipeInput("hmida", 1)
+    inputTopic.pipeInput("lamba", 2)
+
+    println(outputTopic.readRecord().value())
+    println(outputTopic.readRecord().value())
+  }
+
+
+  private def topology: Topology = {
+    val builder = new StreamsBuilder
+    builder
+      .stream[String, Integer]("input-topic", Consumed.`with`(Serdes.String(), Serdes.Integer()))
+      .mapValues(value => new Integer(Math.pow(value.intValue(), 2).intValue()))
+      .to("output-topic", Produced.`with`(Serdes.String(), Serdes.Integer()))
+
+    builder.build()
+  }
+
+  test("window topology") {
+    val sd = setUpDriver(windowTopology, "earnings", "agg-earnings")
+
+    val baseTime = Instant.now().minusSeconds(10)
+
+    sd.inputTopic.pipeInput("hmida", 10, baseTime)
+    sd.inputTopic.pipeInput("hmida", 20, baseTime)
+    sd.inputTopic.pipeInput("hmida", 15, baseTime)
+    sd.inputTopic.pipeInput("lambda", 50, baseTime.plusSeconds(1))
+    sd.inputTopic.pipeInput("hmida", 30, baseTime.plusSeconds(1))
+
+    readOutputTopic(sd.outputTopic)
+  }
+
+  private def windowTopology: Topology = {
+    val builder = new StreamsBuilder
+
+    val windowSize = Duration.ofSeconds(1)
+    val tumblingWindow = TimeWindows.ofSizeWithNoGrace(windowSize).advanceBy(windowSize)
+
+
+    val windowedSerializer = new TimeWindowedSerializer[String](new StringSerializer)
+    val windowedDeserializer = new TimeWindowedDeserializer[String](new StringDeserializer, windowSize.toMillis)
+    val windowedSerde = Serdes.serdeFrom(windowedSerializer, windowedDeserializer)
+
+    val storeSupplier =
+      new RocksDbWindowBytesStoreSupplier(
+        "window_store",
+        Duration.ofSeconds(10).toMillis,
+        Duration.ofSeconds(10).toMillis,
+        windowSize.toMillis,
+        false,
+        false)
+
+    class IntegerAdd extends Reducer[Integer] {
+      override def apply(value1: Integer, value2: Integer): Integer = value1 + value2
+    }
+
+
+    builder
+      .stream[String, Integer]("earnings", Consumed.`with`(Serdes.String(), Serdes.Integer()))
+      .groupByKey()
+      .windowedBy(tumblingWindow)
+      .reduce(
+        new IntegerAdd,
+        Materialized.as(storeSupplier).withKeySerde(Serdes.String()).withValueSerde(Serdes.Integer())
+      )
+      .toStream
+      .to("agg-earnings", Produced.`with`(windowedSerde, Serdes.Integer()))
+
+    builder.build()
+  }
+
+
+  private def setUpDriver(topology: Topology, inputTopicName: String, outputTopicName: String) = {
+    val testDriver: TopologyTestDriver = new TopologyTestDriver(topology)
+    val inputTopic = testDriver.createInputTopic(inputTopicName, new StringSerializer, new IntegerSerializer)
+    val outputTopic = testDriver.createOutputTopic(outputTopicName, new StringDeserializer, new IntegerDeserializer)
+
+    StreamsDriver(testDriver, inputTopic, outputTopic)
+  }
+
+  private case class StreamsDriver(
+                                    driver: TopologyTestDriver,
+                                    inputTopic: TestInputTopic[String, Integer],
+                                    outputTopic: TestOutputTopic[String, Integer]
+                                  )
+
+
+  private def readOutputTopic(topic: TestOutputTopic[String, Integer]): Unit = {
+    while (!topic.isEmpty) {
+      println(topic.readKeyValue())
+    }
+  }
+
+}
